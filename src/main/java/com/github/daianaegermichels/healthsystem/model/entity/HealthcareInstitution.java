ff74c28727184a67545a04d18f662ffd67a3a3df
Daiana Michels <daianaem.12@gmail.com>
2022-07-22 14:52:22 -0300

    feat: creates entities for exam and healthcare institution

--- src/main/java/com/github/daianaegermichels/healthsystem/model/entity/HealthcareInstitution.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/model/entity/HealthcareInstitution.java
@@ -1,4 +1,33 @@
 package com.github.daianaegermichels.healthsystem.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "healthcare_institution")
 public class HealthcareInstitution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name_institution")
+    @NotBlank(message = "Institution name is required")
+    private String nameInstitution;
+
+    @Size(min = 14, max = 14)
+    @NotBlank(message = "CNPJ is required")
+    @CNPJ(message="The CNPJ entered is invalid")
+    private String cnpj;
 }
