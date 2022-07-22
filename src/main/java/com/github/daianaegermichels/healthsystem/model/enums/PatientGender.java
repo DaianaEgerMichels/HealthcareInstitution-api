7dba01f0f99d4214b4fb6042bf6fab2cd6c0c438
Daiana Michels <daianaem.12@gmail.com>
2022-07-22 14:50:24 -0300

    feat: create enum for patient gender

--- src/main/java/com/github/daianaegermichels/healthsystem/model/enums/PatientGender.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/model/enums/PatientGender.java
@@ -1,2 +1,6 @@
-package com.github.daianaegermichels.healthsystem.model.enums;public enum PatientGender {
+package com.github.daianaegermichels.healthsystem.model.enums;
+
+public enum PatientGender {
+    FEMININO,
+    MASCULINO
 }
