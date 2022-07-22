17502eff2f34319686ca2bdbc6a1762205d67884
Daiana Michels <daianaem.12@gmail.com>
2022-07-22 15:20:44 -0300

    feat: add annotations to classes

--- src/main/java/com/github/daianaegermichels/healthsystem/api/controller/HealthcareInstitutionController.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/api/controller/HealthcareInstitutionController.java
@@ -1,4 +1,9 @@
 package com.github.daianaegermichels.healthsystem.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/healthcare-institutions")
 public class HealthcareInstitutionController {
 }
