08d878128dc6ea42390b10b90bb1249c9981e804
Daiana Michels <daianaem.12@gmail.com>
2022-07-23 15:21:35 -0300

    feat: add endpoint to list exams by institution

--- src/main/java/com/github/daianaegermichels/healthsystem/api/controller/ExamController.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/api/controller/ExamController.java
@@ -1,15 +1,22 @@
 package com.github.daianaegermichels.healthsystem.api.controller;
 
 import com.github.daianaegermichels.healthsystem.dto.ExamDTO;
+import com.github.daianaegermichels.healthsystem.model.entity.Exam;
 import com.github.daianaegermichels.healthsystem.service.ExamService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @Api(value = "API REST Exams")
@@ -35,4 +42,16 @@ public class ExamController {
 
         return ResponseEntity.created(location).body(examDTO);
     }
+
+    @GetMapping("/{id_institution}")
+    @ApiOperation(value = "List exams by Healthcare Institution")
+    public ResponseEntity<List<Exam>>getAllExams(@PageableDefault(page = 0, size= 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                                                 @PathVariable(name="id_institution") Long idHealthcareInstitution){
+        var examsList = examService.listAll(idHealthcareInstitution, pageable);
+        if(examsList.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(examsList);
+   }
+
 }
