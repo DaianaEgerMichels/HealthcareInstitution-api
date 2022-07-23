5a3ab0ad7891d15b331f250dcc6ca157aeb091ab
Daiana Michels <daianaem.12@gmail.com>
2022-07-23 12:17:57 -0300

    feat: add save and get by Id methods

--- src/main/java/com/github/daianaegermichels/healthsystem/service/HealthcareInstitutionService.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/service/HealthcareInstitutionService.java
@@ -1,7 +1,38 @@
 package com.github.daianaegermichels.healthsystem.service;
 
+import com.github.daianaegermichels.healthsystem.model.entity.HealthcareInstitution;
+import com.github.daianaegermichels.healthsystem.repository.HealthcareInstitutionRepository;
+import com.github.daianaegermichels.healthsystem.service.exception.HealthcareInstitutionExistsException;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class HealthcareInstitutionService {
+
+    private HealthcareInstitutionRepository healthcareInstitutionRepository;
+
+    public HealthcareInstitutionService(HealthcareInstitutionRepository healthcareInstitutionRepository){
+        this.healthcareInstitutionRepository = healthcareInstitutionRepository;
+    }
+
+    @Transactional
+    public void save(HealthcareInstitution healthcareInstitution){
+        isUniqueHealthcareInstitution(healthcareInstitution);
+        healthcareInstitutionRepository.save(healthcareInstitution);
+    }
+
+    public Optional<HealthcareInstitution> getById(Long id){
+        return healthcareInstitutionRepository.findById(id);
+    }
+
+    private void isUniqueHealthcareInstitution(HealthcareInstitution healthcareInstitution) {
+        var optionalHealthcareInstitution = healthcareInstitutionRepository
+                .findByCnpj(healthcareInstitution.getCnpj());
+
+        if(optionalHealthcareInstitution.isPresent()) {
+            throw new HealthcareInstitutionExistsException("There is already a Healthcare Institution registered with this CNPJ!");
+        }
+    }
 }
