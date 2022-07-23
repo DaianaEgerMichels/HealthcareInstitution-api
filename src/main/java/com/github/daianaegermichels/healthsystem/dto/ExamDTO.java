214a0bde85ea3d65f3f5dd11546d74d92b71dcfd
Daiana Michels <daianaem.12@gmail.com>
2022-07-23 10:47:10 -0300

    feat: add required input data

--- src/main/java/com/github/daianaegermichels/healthsystem/dto/ExamDTO.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/dto/ExamDTO.java
@@ -1,4 +1,40 @@
 package com.github.daianaegermichels.healthsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExamDTO {
+
+    private Long id;
+
+    @NotNull(message = "Healthcare Institution Id is required")
+    private Long institutionId;
+
+    @NotBlank(message = "The patient's name is required")
+    private String patientName;
+
+    @NotNull(message = "The patient's age is required")
+    private Integer patientAge;
+
+    @NotBlank(message = "The patient's gender is required")
+    private String patientGender;
+
+    @NotBlank(message = "Physician name is required")
+    private String physicianName;
+
+    @NotBlank(message = "Physician's CRM number is required")
+    private String physicianCRM;
+
+    @NotBlank(message = "Procedure name is required")
+    private String procedureName;
 }
