dd6187c9a02f2f696ff3ecddb8617934c281300e
Daiana Michels <daianaem.12@gmail.com>
2022-07-25 20:26:00 -0300

    feat: adds exception for insufficient Pixeon Coins

--- src/main/java/com/github/daianaegermichels/healthsystem/api/handler/InsufficientPixeonCoinsHandler.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/api/handler/InsufficientPixeonCoinsHandler.java
@@ -1,2 +1,24 @@
-package com.github.daianaegermichels.healthsystem.api.handler;public class InsufficientPixeonCoinsHandler {
+package com.github.daianaegermichels.healthsystem.api.handler;
+
+import com.github.daianaegermichels.healthsystem.dto.ErrorResponse;
+import com.github.daianaegermichels.healthsystem.service.exception.InsufficientPixeonCoinsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class InsufficientPixeonCoinsHandler {
+    @ExceptionHandler({InsufficientPixeonCoinsException.class })
+    public ResponseEntity<ErrorResponse> insufficientPixeonCoinsException(InsufficientPixeonCoinsException e) {
+
+        ErrorResponse error = new ErrorResponse();
+        error.setCode(HttpStatus.PAYMENT_REQUIRED.value());
+        error.setTimestamp(LocalDateTime.now());
+        error.getMessages().add(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.PAYMENT_REQUIRED).body(error);
+    }
 }
--- src/main/java/com/github/daianaegermichels/healthsystem/service/exception/InsufficientPixeonCoinsException.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/service/exception/InsufficientPixeonCoinsException.java
@@ -1,2 +1,7 @@
-package com.github.daianaegermichels.healthsystem.service.exception;public class InsufficientPixeonCoinsException {
+package com.github.daianaegermichels.healthsystem.service.exception;
+
+public class InsufficientPixeonCoinsException extends RuntimeException{
+    public InsufficientPixeonCoinsException(String message){
+        super(message);
+    }
 }
