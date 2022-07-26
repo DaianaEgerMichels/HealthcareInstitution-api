30e07a8d5483919b36d15186711887f9df7f7a88
Daiana Michels <daianaem.12@gmail.com>
2022-07-25 22:34:07 -0300

    feat: adds dto class as input parameter

--- src/main/java/com/github/daianaegermichels/healthsystem/api/controller/HealthcareInstitutionController.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/api/controller/HealthcareInstitutionController.java
@@ -1,6 +1,6 @@
 package com.github.daianaegermichels.healthsystem.api.controller;
 
-import com.github.daianaegermichels.healthsystem.model.entity.HealthcareInstitution;
+import com.github.daianaegermichels.healthsystem.dto.HealthcareInstitutionDTO;
 import com.github.daianaegermichels.healthsystem.service.HealthcareInstitutionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -25,15 +25,15 @@ public class HealthcareInstitutionController {
 
     @PostMapping
     @ApiOperation(value = "Create a new Healthcare Institution")
-    public ResponseEntity<HealthcareInstitution> createHealthcareInstitution(@Valid @RequestBody HealthcareInstitution healthcareInstitution) {
+    public ResponseEntity<HealthcareInstitutionDTO> createHealthcareInstitution(@Valid @RequestBody HealthcareInstitutionDTO healthcareInstitutionDTO) {
 
-        healthcareInstitutionService.save(healthcareInstitution);
+        healthcareInstitutionService.save(healthcareInstitutionDTO);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(healthcareInstitution.getId()).toUri();
+                .buildAndExpand(healthcareInstitutionDTO.getId()).toUri();
 
-        return ResponseEntity.created(location).body(healthcareInstitution);
+        return ResponseEntity.created(location).body(healthcareInstitutionDTO);
     }
 
     @GetMapping("/{id}")
