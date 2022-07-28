c261e0d210eb1b1795aeee3fb42dccd19b43336e
Daiana Michels <daianaem.12@gmail.com>
2022-07-27 22:21:42 -0300

    feat: change CORS enable mode

--- src/main/java/com/github/daianaegermichels/healthsystem/HealthSystemApplication.java
+++ src/main/java/com/github/daianaegermichels/healthsystem/HealthSystemApplication.java
@@ -2,18 +2,10 @@ package com.github.daianaegermichels.healthsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class HealthSystemApplication implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-    }
+public class HealthSystemApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(HealthSystemApplication.class, args);
